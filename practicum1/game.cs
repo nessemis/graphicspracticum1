ef43bb2fd4129ad2635817e71e5a733a8e563f24
nessemis <dennis.ar5@gmail.com>
2017-04-29 19:12:40 +0200

    rotate

--- practicum1/game.cs
+++ practicum1/game.cs
@@ -5,6 +5,8 @@ namespace Template {
 
     class Game
     {
+        private float time = 0f;
+
         // member variables
         public Surface screen;
         // initialize
@@ -14,6 +16,8 @@ namespace Template {
         // tick: renders one frame
         public void Tick()
         {
+            time += 0.0005f;
+
             screen.Clear(0);
 
             int x = screen.width / 2 - 128;
@@ -25,11 +29,32 @@ namespace Template {
                 for (int j = y; j < 256 + y; j++)
                 {
                     int c2 = j - y;
-                    screen.pixels[i+ j * screen.width] = (c << 16 )+ (c2<<8) ;
-
+                    screen.pixels[RotatedScreenCoords(time, i, j)] = (c << 16 )+ (c2<<8) ;
                 }
             }
+        }
 
+        private int RotatedScreenCoords(float a, int x, int y)
+        {
+            int rotCenterX = screen.width / 2;
+            int rotCenterY = screen.height / 2;
+
+            int rotX = TX(a, x - rotCenterX, y - rotCenterY) + rotCenterX;
+            int rotY = TY(a, x - rotCenterX, y - rotCenterY) + rotCenterY;
+
+            return rotX + rotY * screen.width;
+        }
+
+        //transforms coordinates to a rotated version
+        private int TX (float a, int x, int y)
+        {
+            return (int) (Math.Cos(a) * x - Math.Sin(a) * y);
+        }
+
+        //transforms coordinates to a rotated version
+        private int TY(float a, int x, int y)
+        {
+            return (int) (Math.Sin(a) * x + Math.Cos(a) * y);
         }
     }
 } // namespace Template
