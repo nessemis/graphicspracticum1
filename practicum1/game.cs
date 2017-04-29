fa4e7519cc20fea03bb199f8bd10c1a50c15c8c0
Doycie <imcody97@gmail.com>
2017-04-29 15:32:06 +0200

    Excersise1 first half

--- practicum1/game.cs
+++ practicum1/game.cs
@@ -3,21 +3,31 @@ using System.IO;
 
 namespace Template {
 
-class Game
-{
-	// member variables
-	public Surface screen;
-	// initialize
-	public void Init()
-	{
-	}
-	// tick: renders one frame
-	public void Tick()
-	{
-		screen.Clear( 0 );
-		screen.Print( "hello world", 2, 2, 0xffffff );
-        screen.Line(2, 20, 160, 20, 0xff0000);
-	}
-}
+    class Game
+    {
+        // member variables
+        public Surface screen;
+        // initialize
+        public void Init()
+        {
+        }
+        // tick: renders one frame
+        public void Tick()
+        {
+            screen.Clear(0);
 
+            int x = screen.width / 2 - 128;
+            int y = screen.height / 2 - 128;
+
+            for(int i = x; i < 256 + x; i++)
+            {
+                for (int j = y; j < 256 + y; j++)
+                {
+                    screen.pixels[i+ j * screen.width] = 255;
+
+                }
+            }
+
+        }
+    }
 } // namespace Template
