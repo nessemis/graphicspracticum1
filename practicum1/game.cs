4f94fbe80c44f372e3097d75c69c0734e3a9f8ac
nessemis <dennis.ar5@gmail.com>
2017-05-01 01:46:58 +0200

    made the better worse, but it should be perfect for the exersize
    # Conflicts:
    #	practicum1/game.cs

--- practicum1/game.cs
+++ practicum1/game.cs
@@ -1,112 +1,101 @@
 ﻿using System;
-using System.IO;
 
-namespace Template {
-
-
+namespace Template
+{
     class Game
     {
-        
-        public static int width;
-        public static  int height;
 
-        public class vec2
+
+        public struct vec2
         {
             public float x;
             public float y;
-            public void rotate(float a)
-            {
-                float tempx = x;
-                 x = (float)(x * Math.Cos(a) - y * Math.Sin(a) );
-                 y = (float)(tempx * Math.Sin(a) + y * Math.Cos(a) );
-            }
             public vec2(float x, float y)
             {
                 this.x = x;
                 this.y = y;
             }
+
+            public vec2 rotate(float angle, vec2 rotationOrigin)
+            {
+                float rotatedX = (float)(Math.Cos(angle) * (x - rotationOrigin.x) - Math.Sin(angle) * (y - rotationOrigin.y) + rotationOrigin.x);
+                float rotatedY = (float)(Math.Sin(angle) * (x - rotationOrigin.x) + Math.Cos(angle) * (y - rotationOrigin.y) + rotationOrigin.y);
+
+                return new vec2(rotatedX, rotatedY);
+            }
+
+            public static vec2 Origin
+            {
+                get
+                {
+                    return new vec2(0, 0);
+                }
+            }
         }
-       
-        public class quad {
+
+        public struct quad
+        {
             public vec2 tl;
             public vec2 tr;
             public vec2 bl;
             public vec2 br;
 
-
-
-          public void rotate(float a)
+            public quad rotate(float angle, vec2 rotationOrigin)
             {
-                tl.rotate(a);
-                tr.rotate(a);
-                bl.rotate(a);
-                br.rotate(a);
+                vec2 rotTl = tl.rotate(angle, rotationOrigin);
+                vec2 rotTr = tr.rotate(angle, rotationOrigin);
+                vec2 rotBr = br.rotate(angle, rotationOrigin);
+                vec2 rotBl = bl.rotate(angle, rotationOrigin);
+
+                return new quad(rotTl, rotTr, rotBr, rotBl);
             }
 
-            public  quad(vec2 tl, vec2 tr,vec2 br, vec2 bl)
+            public quad(vec2 tl, vec2 tr, vec2 br, vec2 bl)
             {
                 this.tl = tl;
                 this.tr = tr;
                 this.br = br;
                 this.bl = bl;
             }
-            public void draw(Surface screen)
-            {
-                screen.Line(TX(tl.x), TY(tl.y), TX(tr.x), TY(tr.y), 0xff0000);
-                screen.Line(TX(tr.x), TY(tr.y), TX(br.x), TY(br.y), 0x00ff00);
-                screen.Line(TX(br.x), TY(br.y), TX(bl.x), TY(bl.y), 0x0000ff);
-                screen.Line(TX(bl.x), TY(bl.y), TX(tl.x), TY(tl.y), 0xffffff);
-            }
-
-            public int TX(float x)
-            {
-                return (int)((x + 1.0f) * (Game.width / 4));
-            }
-
-            public int TY(float y)
-            {
-                return (int)((-y + 1.0f) * (Game.width * (Game.width / Game.height) / 4));
-            }
-
         }
         // member variables
         public Surface screen;
-        quad[] b;
+        quad b;
         float a = 0;
         // initialize
         public void Init()
         {
-            Game.width = screen.width;
-            Game.height = screen.height;
-            Random rnd = new Random();
-            b = new quad[5];
-            for(int i =0;i<5;i++ )
-
-            b[i] = new quad(new vec2((float)-rnd.NextDouble(), (float)rnd.NextDouble()), new vec2((float)rnd.NextDouble(), (float)rnd.NextDouble()), 
-                new vec2((float)rnd.NextDouble(), -(float)rnd.NextDouble()), new vec2(-(float)rnd.NextDouble(), -(float)rnd.NextDouble()));
-
-           
+            b = new quad(new vec2(-0.5f, 0.5f), new vec2(0.5f, 0.5f), new vec2(0.5f, -0.5f), new vec2(-0.5f, -0.5f));
         }
         // tick: renders one frame
         public void Tick()
         {
-         
             screen.Clear(0);
-            a += 0.005f;
 
-            screen.Line(220, 100, 420, 100, 0x00ffff);
-            screen.Line(220, 100, 220, 300, 0x00ffff);
-            screen.Line(420, 100, 420, 300, 0x00ffff);
-            screen.Line(220, 300, 420, 300, 0x00ffff);
-           
-            for (int i = 0;i<5;i++)
-            {
-                b[i].rotate(a);
-                b[i].draw(screen);
-            }
+            a += 0.05f;
+
+            DrawRotatingQuad(b, a);
         }
 
-    
+        public void DrawRotatingQuad(quad q, float angle)
+        {
+            quad rq = q.rotate(angle, vec2.Origin);
+
+            screen.Line(TX(rq.tl.x), TY(rq.tl.y), TX(rq.tr.x), TY(rq.tr.y), 0xff0000);
+            screen.Line(TX(rq.tr.x), TY(rq.tr.y), TX(rq.br.x), TY(rq.br.y), 0x00ff00);
+            screen.Line(TX(rq.br.x), TY(rq.br.y), TX(rq.bl.x), TY(rq.bl.y), 0x0000ff);
+            screen.Line(TX(rq.bl.x), TY(rq.bl.y), TX(rq.tl.x), TY(rq.tl.y), 0xffffff);
+        }
+
+        public int TX(float x)
+        {
+            return (int)((x + 2.0f) * (screen.width / 4));
+        }
+
+        public int TY(float y)
+        {
+            return (int)(((-y * (screen.width / (float)screen.height) + 2.0f)) * (screen.height / 4));
+        }
 
 
     }
