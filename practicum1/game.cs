87d3edcff6b20979b642dd2cc7348406a3084a9e
nessemis <dennis.ar5@gmail.com>
2017-05-02 16:02:49 +0200

    reset (#4)

--- practicum1/game.cs
+++ practicum1/game.cs
@@ -4,8 +4,6 @@ namespace Template
 {
     class Game
     {
-
-
         public struct vec2
         {
             public float x;
@@ -58,45 +56,18 @@ namespace Template
                 this.bl = bl;
             }
         }
+
         // member variables
         public Surface screen;
-        quad b;
-        float a = 0;
+
         // initialize
         public void Init()
         {
-            b = new quad(new vec2(-0.5f, 0.5f), new vec2(0.5f, 0.5f), new vec2(0.5f, -0.5f), new vec2(-0.5f, -0.5f));
         }
         // tick: renders one frame
         public void Tick()
         {
             screen.Clear(0);
-
-            a += 0.05f;
-
-            DrawRotatingQuad(b, a);
         }
-
-        public void DrawRotatingQuad(quad q, float angle)
-        {
-            quad rq = q.rotate(angle, vec2.Origin);
-
-            screen.Line(TX(rq.tl.x), TY(rq.tl.y), TX(rq.tr.x), TY(rq.tr.y), 0xff0000);
-            screen.Line(TX(rq.tr.x), TY(rq.tr.y), TX(rq.br.x), TY(rq.br.y), 0x00ff00);
-            screen.Line(TX(rq.br.x), TY(rq.br.y), TX(rq.bl.x), TY(rq.bl.y), 0x0000ff);
-            screen.Line(TX(rq.bl.x), TY(rq.bl.y), TX(rq.tl.x), TY(rq.tl.y), 0xffffff);
-        }
-
-        public int TX(float x)
-        {
-            return (int)((x + 2.0f) * (screen.width / 4));
-        }
-
-        public int TY(float y)
-        {
-            return (int)(((-y * (screen.width / (float)screen.height) + 2.0f)) * (screen.height / 4));
-        }
-
-
     }
 }
