b59562eb0f77919fc9cb686e428e2ac525a75ccc
nessemis <dennis.ar5@gmail.com>
2017-05-05 19:41:17 +0200

    cleansed

--- practicum1/shaders/fs.glsl
+++ practicum1/shaders/fs.glsl
@@ -1,8 +1,12 @@
+//FRAGMENT SHADER
 #version 330
 in vec4 color;
+
+//normal vector of this vertex.
 in vec4 normal;
 out vec4 outputColor;
 void main()
 {
+ //light rays drop straight down onto the surface (hence vec4(0, 0, -1, 0)).
  outputColor = color * dot(normal, vec4(0, 0, -1, 0));
 }
--- practicum1/shaders/vs.glsl
+++ practicum1/shaders/vs.glsl
@@ -1,11 +1,17 @@
+//VERTEX SHADER
 #version 330
 in vec3 vPosition;
 in vec3 vNormal;
 in vec3 vColor;
 out vec4 color;
 out vec4 normal;
-uniform mat4 M;
+
+//Scales the landscape to sizes that fit in screen-space (and color-space).
 uniform mat4 S;
+
+//Transforms the position in screen-space.
+uniform mat4 M;
+
 void main()
 {
  gl_Position = M * S * vec4( vPosition, 1.0 );
